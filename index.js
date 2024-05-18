76e9d4be3e48ad03010afd948f6e7c23d6d9c2aa
mumtozBEK1234 <email manzilingiz>
2024-05-18 18:50:25 +0300

    4-oy 2dars

---
+++ index.js
@@ -0,0 +1,123 @@
+ // 1
+// let a = 7;
+//   if (a > 0) {
+//     a++;
+//   }
+
+//   console.log(a);
+
+// 2
+// let b = 9;
+//   if (b > 0 ) {
+//     b -= 2;
+//   }
+
+//   console.log(b);
+
+
+
+
+
+
+
+// 4
+// let a =+ prompt("a ni kiriting");
+// let m =+ prompt("m ni kiriting");
+// let l = a/m ;
+// o =parseInt(o);
+// document.write(o);
+
+// 5
+
+// 6
+// let number =+ prompt("ikki xonali son kiriting");
+// let l = number/10;
+// let m = number%10;
+// l =preseInt(l);
+// m =preseInt(m);
+// document.write(l);
+// document.write("va");
+// document.write(m);
+
+
+// 7
+// let number =+prompt("ikki xonali son kiriting");
+//let w = number%10; 
+// let o = number/10;
+// w =pareseInt(m);
+// o =pareseInt(n);
+// document.write(w+o);
+
+
+
+// // 8
+// let b = 20
+// let a = 10
+// if (a > 10) {
+//     20;
+
+   
+// }
+// if (20 > a) {
+//     10;
+// }
+
+
+// if (10 > 20)
+// console.log(b);
+
+
+// 9
+// let a = 5;
+// let b = 6;
+//  if (a > b) {
+//     5;
+//  }
+//  if (b > a) {
+//     6;
+//  }
+//  console.log(a = b);
+
+// 12
+// let a = 1;
+// let b = 2;
+// let v = 3;
+// if (b > a ) {
+//     1;
+// }
+// if (v < a) {
+//     1;
+// }
+
+
+// console.log(a);
+
+// 13
+// let a = 1;
+// let b = 2;
+// let v = 3;
+// if (b > a ) {
+//     2;
+// }
+// if (v < b) {
+//     2;
+// }
+
+// console.log(b);
+
+// 14
+// let b = 10;
+// let a = 11;
+// let v = 20;
+// if (b < a) {
+//     10;
+// }
+// if (a > v) {
+//     11;
+// }
+
+// if (v > b) {
+//     20;
+// }
+
+// console.log(b,a,v);
